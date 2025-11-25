b24db47ed6b9cc1710bab14de506ad92f2b6dbf6
Stan Van den heuvel <vandenheuvelstan@hotmail.com>
2025-11-25 14:21:45 +0100

    google maps

--- components/specificComponents/Location/Location.js
+++ components/specificComponents/Location/Location.js
@@ -25,8 +25,18 @@ export default class Location extends Component {
 								<h2 className={css["rich-text-section__title"]}>Location Details</h2>
 								<div className={css["rich-text-section__rich-text"]}>{RichTextToHTML({ document: this.props.blok.description })}</div>
 							</section>
-						</div>
-					</div>
+						</div>  {/* Google Maps iframe */}
+<iframe
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5017.11723708637!2d3.7247649765242845!3d51.04277234453954!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c37150c174667b%3A0x8f31c1404ab55e14!2sFaculteit%20Economie%20en%20Bedrijfskunde%2C%20Tweekerkenstraat%202%2C%209000%20Gent!5e0!3m2!1snl!2sbe!4v1764075172616!5m2!1snl!2sbe"
+  width="600"
+  height="450"
+  style={{ border: 0 }}
+  allowFullScreen
+  loading="lazy"
+  referrerPolicy="no-referrer-when-downgrade"
+></iframe>
+
+					</div> 
 					{this.props.blok.additionalstuff && this.props.blok.additionalstuff.map((nestedBlok) => (
 							<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
 						))}
