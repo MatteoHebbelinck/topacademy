bf1fc9fb038cefbdc5200c7951b595ad7a9fd130
MatteoHebbelinck <matteo.hebbelinck@ugent.be>
2025-12-01 18:14:09 +0100

    google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -16,3 +16,34 @@ export default function Document() {
     </Html>
   );
 }
+// /pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="nl">
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-2RELL1FX4N"
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-2RELL1FX4N');
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
